c5b40378f3f0a77743bd6d84316eab52cf112823
donovanroubos <hello@donovanroubos.nl>
2021-01-21 15:44:47 +0100

    Replace Tabletop with GoogleAPI sheets functionality and compress bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,39 @@
-import json from 'rollup-plugin-json';
-import babel from 'rollup-plugin-babel';
+import json from 'rollup-plugin-json'
+import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
-import resolve from 'rollup-plugin-node-resolve';
+import resolve from 'rollup-plugin-node-resolve'
 import smartAsset from 'rollup-plugin-smart-asset'
+import minify from 'rollup-plugin-babel-minify'
+
+const dev = 'development'
+const prod = 'production'
+const env = (process.env.NODE_ENV === prod || process.env.NODE_ENV === dev) ? process.env.NODE_ENV : dev
+
+const plugins = [
+  resolve(),
+  commonjs(),
+  babel({
+    exclude: 'node_modules/**',
+  }),
+  json({ compact: true, }),
+  smartAsset({
+    url: 'copy',
+    assetsPath: 'flags',
+    useHash: true,
+    keepImport: true
+  })
+]
+
+if (env === prod) {
+  plugins.push(minify())
+}
 
 export default {
+  plugins,
   input: 'index.js',
   output: {
     file: 'dist/bundle.js',
     format: 'cjs',
     name: 'flagpack-core'
-  },
-  plugins: [
-    resolve(),
-    commonjs(),
-    babel({
-      exclude: 'node_modules/**',
-    }),
-    json({ compact: true, }),
-    smartAsset({
-      url: 'copy', 
-      assetsPath: 'flags',
-      useHash: true,
-      keepImport: true
-    })
-  ]
-};
+  }
+}
